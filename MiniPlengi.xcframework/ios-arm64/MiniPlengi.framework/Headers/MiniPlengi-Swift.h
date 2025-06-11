17060245db40ca491bdd32af7a86c0dc5fe27bba
junplat <pgj0320@loplat.com>
2025-06-11 15:22:34 +0900

    feat: v1.5.10-rc2

--- MiniPlengi.xcframework/ios-arm64/MiniPlengi.framework/Headers/MiniPlengi-Swift.h
+++ MiniPlengi.xcframework/ios-arm64/MiniPlengi.framework/Headers/MiniPlengi-Swift.h
@@ -513,16 +513,6 @@ SWIFT_CLASS("_TtC10MiniPlengi6Plengi")
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 @end
 
-
-
-@interface Plengi (SWIFT_EXTENSION(MiniPlengi))
-/// 발급받은 FCM 토큰을 기기와 로플랫 서버에 저장합니다.
-/// 토큰을 처음 발급받거나 새로 발급받는 상황에서만 저장합니다.
-/// \param fcmToken 발급받은 FCM 토큰
-///
-+ (void)registerFcmWithFcmToken:(NSString * _Nullable)fcmToken;
-@end
-
 @class UNUserNotificationCenter;
 @class UNNotificationResponse;
 enum Result : NSInteger;
